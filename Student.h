24983a68f0c021edd7df62213c6e71019c1e7020
BugLeesir <1007878154@qq.com>
2022-11-11 14:40:28 +0800

    1

---
+++ Student.h
@@ -0,0 +1,21 @@
+#include<string>
+using namespace std;
+class Student
+{
+private:
+    /* data */
+    string name;
+    int age;
+public:
+    Student(/* args */);
+    ~Student();
+
+};
+
+Student::Student(/* args */)
+{
+}
+
+Student::~Student()
+{
+}
